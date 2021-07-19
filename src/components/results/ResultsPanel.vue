103574bd1b6b37e9ff4726cbd7a5ec430e070724
Olivier Filangi <olivier.filangi@gmail.com>
2021-07-19 10:02:01 +0200

    button results/property select

---
+++ src/components/results/ResultsPanel.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="results-panel">
+
+    {{ 
+        requestManager.serialized()
+    }}
+</div>
+</template>
+
+<script lang="ts">
+import RequestManager from '@/ts/RequestManager';
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    name: "ResultsPanel",
+
+    props: {
+        rm : String
+    },
+
+    methods: {
+    },
+
+    data() { 
+        return {
+           
+        }
+    },
+
+    computed: {
+        requestManager() {
+            return new RequestManager(this.rm)
+        }
+    },
+})
+
+export default class ResultsPanel extends Vue {
+
+}
+
+
+</script>
+
+<style>
+
+</style>
